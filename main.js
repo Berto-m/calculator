106e8517a9a8f9f2363a609fa30d29b89104bb6c
Alberto <medina.v@pm.me>
2023-08-01 20:26:56 +0100

    display numbers on keydown in the result container

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const resultNumbers = document.querySelector(".result-numbers");
-const numbers = document.querySelector('.numbers')
+const numbers = document.querySelectorAll(".numbers");
 
-const displayResultNumbers = (event) => {
-  console.log("nothing");
-  if (
-    resultNumbers.textContent.length > 7 &&
-    resultNumbers.textContent.length < 12
-  ) {
-    resultNumbers.style.fontSize = "2.5rem";
-  }
+let firstNum = 0
+let operator = undefined
+let secondNum = 0
+let mainResult = ''
+
+console.log(operator);
+
+console.log("changing font size so the result doesn't overflow");
+if (resultNumbers.textContent.length > 7 ) {
+  resultNumbers.style.fontSize = "2.4rem";
+}
+
+
+const clickKeydownHandler = (event) => {
+  numbers.forEach((number) => {
+    if (event.type === "keydown" && event.key === number.value) {
+      console.log(number.value);
+      mainResult += number.value
+      resultNumbers.textContent = mainResult
+      console.log(`I am the final result ${mainResult}`);
+    } else if (event.type === "click" && event.target.value === number.value) {
+      console.log(number);
+    }
+  });
 };
 
-
+document.addEventListener("keydown", clickKeydownHandler);
+document.addEventListener("click", clickKeydownHandler);
