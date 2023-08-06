9a2dec8a6f172917062f8a332d8f96a109af9a5f
Alberto <medina.v@pm.me>
2023-08-06 22:43:09 +0100

    add chain operators functionly but still incomplete

--- main.js
+++ main.js
@@ -1,49 +1,68 @@
 const resultNumbers = document.querySelector(".result-numbers");
-const resultOperations = document.querySelector('.result-operations')
+const resultOperations = document.querySelector(".result-operations");
 const allBtns = document.querySelectorAll(
   ".numbers-and-operators-container input"
 );
 const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-const allOperations = ["%", "/", "*", 'x', "-", "+"];
+const allOperations = ["%", "/", "*", "x", "-", "+"];
 let firstNum = "";
 let operators = "";
 let secondNum = "";
-let mainResult = "";
+let mainResult = 0;
 
 if (resultNumbers.textContent.length > 7) {
   resultNumbers.style.fontSize = "2.4rem";
 }
 
 const mainResultHandler = () => {
-  // if (firstNum && secondNum) {
-    
-  // }
+  if ((operators === "x" || operators === "*") && !mainResult) {
+    mainResult = parseInt(firstNum) * parseInt(secondNum);
+  } else if (mainResult && (operators === "x" || operators === "*")) {
+    mainResult = mainResult * parseInt(secondNum);
+  }
+  console.log(mainResult);
 };
 
-const updateNumbers = (number) => {
+// It needs check if there's already an operators in place
+// to update first or second second number. 
+const updateNumbersHandler = (number) => {
   if (!operators) {
     firstNum += number;
-    resultNumbers.textContent = firstNum
-    console.log('first number');
+    resultNumbers.textContent = firstNum;
   } else {
     secondNum += number;
-    resultNumbers.textContent = secondNum
-    console.log('second number');
+    resultNumbers.textContent = secondNum;
+    mainResultHandler();
   }
-}
+};
 
-const emptyAll = () => {
+const acButtonHandler = () => {
   firstNum = "";
   operators = "";
   secondNum = "";
-  mainResult = "";
-  resultNumbers.textContent = ''
-  resultOperations.textContent = ''
-}
+  mainResult = 0;
+  resultNumbers.textContent = "";
+  resultOperations.textContent = "";
+};
 
 const negateCurrentValue = () => {
-  console.log('do something');
-}
+  console.log("do something here");
+};
+
+// Must only work when a number is entered.
+// SecondNum must be emptied here so when a new operator is clicked
+// or pressed the mainResultHandler() doesn't use the old values from 
+// from SecondNum.can 
+const operatorsHandler = (currentOperator) => {
+  if (operators) {
+    operators = currentOperator;
+    secondNum = "";
+    console.log(operators);
+  } else if (firstNum) {
+    operators = currentOperator;
+    console.log(operators);
+  }
+};
 
 // checks if it's a number or operator
 const clickKeydownHandler = (event) => {
@@ -52,20 +71,24 @@ const clickKeydownHandler = (event) => {
       allNumbers.includes(event.target.value) ||
       allNumbers.includes(event.key)
     ) {
-      updateNumbers(event.target.value ?? event.key)
-    } else if (event.target.value === 'AC') {
-      emptyAll()
-    } else if (event.target.value === '+/-') {
-      negateCurrentValue()
+      updateNumbersHandler(event.target.value ?? event.key);
+    } else if (event.target.value === "AC") {
+      acButtonHandler();
+    } else if (event.target.value === "+/-") {
+      negateCurrentValue();
     } else if (
       allOperations.includes(event.target.value) ||
       allOperations.includes(event.key)
     ) {
-      operators += event.target.value ?? event.key;
+      operatorsHandler(event.target.value ?? event.key);
+    } else if (event.target.value == "=") {
+      console.log("hello");
     }
   }
-  
 };
 
+
+// It needs read whole page otherwise the buttons/inputs
+// would need to be in an active state.
 document.addEventListener("keydown", clickKeydownHandler);
 document.addEventListener("click", clickKeydownHandler);
