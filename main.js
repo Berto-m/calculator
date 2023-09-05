e45a336ab70e7ce0732751bddfa1d25efda8baaf
Alberto <medina.v@pm.me>
2023-09-05 22:54:07 +0100

    refactor functions

--- main.js
+++ main.js
@@ -5,145 +5,153 @@ const decimalBtn = document.querySelector(".decimal");
 const allBtns = document.querySelectorAll(
   ".numbers-and-operators-container input"
 );
-const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-const allOperations = ["%", "/", "*", "x", "-", "+", "=", "AC", "+/-", "Enter"];
+const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const allOperations = ["%", "/", "*", "x", "-", "+"];
 let firstNum = "";
-let operators = "";
+let operator = "";
 let secondNum = "";
-let mainResult = 0;
 
-const checkNumLength = (num) => {
-  num = num.toString();
+const deleteAll = () => {
+  firstNum = "";
+  operator = "";
+  secondNum = "";
+  resultOperations.textContent = "";
+  resultNumbers.textContent = "";
+};
+
+const styleCurrentNumber = (num) => {
   if (num.length >= 0 && num.length <= 6) {
     resultNumbers.style.fontSize = "3.5rem";
   } else if (num.length === 8) {
     resultNumbers.style.fontSize = "3rem";
   } else if (num.length > 8) {
-    resultNumbers.style.fontSize = "2.4rem";
+    resultNumbers.style.fontSize = "2.2rem";
   }
   return num;
 };
 
-// SecondNum is emptied here otherwise it add the new numbers
-// to the old ones.
-const mainResultHandler = (num1 = firstNum, num2 = secondNum) => {
+const calculateResult = (num1, action, num2) => {
   num1 = parseFloat(num1);
   num2 = parseFloat(num2);
+  firstNum = parseFloat(firstNum);
 
-  if (operators !== "%") {
-    if (!mainResult) {
-      resultOperations.textContent = `${num1} ${operators} ${num2}`;
-    } else {
-      resultOperations.textContent = `${mainResult} ${operators} ${num2}`;
-    }
+  if (action === "/") {
+    firstNum = num1 / num2;
+  } else if (action === "x" || action === "*") {
+    firstNum = num1 * num2;
+  } else if (action === "-") {
+    firstNum = num1 - num2;
+  } else if (action === "+") {
+    firstNum = num1 + num2;
   }
 
-  if (!mainResult && (operators === "x" || operators === "*")) {
-    mainResult = num1 * num2;
-  } else if (mainResult && (operators === "x" || operators === "*")) {
-    mainResult = mainResult * num2;
-  } else if (!mainResult && operators === "+") {
-    mainResult = num1 + num2;
-  } else if (mainResult && operators === "+") {
-    mainResult = mainResult + num2;
-  } else if (!mainResult && operators === "-") {
-    mainResult = num1 - num2;
-  } else if (mainResult && operators === "-") {
-    mainResult = mainResult - num2;
-  } else if (!mainResult && operators === "%") {
-    mainResult = num1 / num2;
-  } else if (mainResult && operators === "%") {
-    mainResult = mainResult / num2;
-  } else if (!mainResult && operators === "/") {
-    mainResult = num1 / num2;
-  } else if (mainResult && operators === "/") {
-    mainResult = mainResult / num2;
+  if (firstNum > 999999999 || firstNum < -999999999) {
+    firstNum = firstNum.toExponential(2);
+  } else if (firstNum % 2 !== 0) {
+    firstNum = Math.round((firstNum + Number.EPSILON) * 1000) / 1000;
   }
 
-  if (mainResult > 999999999 || mainResult < -999999999) {
-    mainResult = mainResult.toExponential();
-    resultNumbers.textContent = `${checkNumLength(mainResult)}`;
-  } else if (Number.isInteger(mainResult)) {
-    resultNumbers.textContent = `${checkNumLength(mainResult)}`;
-  } else {
-    mainResult = Math.round((mainResult + Number.EPSILON) * 100) / 100;
-    resultNumbers.textContent = `${checkNumLength(mainResult)}`;
-  }
+  firstNum = firstNum.toString();
   secondNum = "";
-  
 };
 
-// It needs check if there's already an operators in place
-// to update first or second second number.
-// decimal must disabled after it's been clicked
-const updateNumbersHandler = (number) => {
- if (!operators && firstNum.length < 10) {
-    if (number === ".") {
-      if (!firstNum) {
-        firstNum += "0";
-        resultNumbers.textContent = `${checkNumLength(firstNum)}`;
-        decimalBtn.disabled = true;
-      }
-    }
-    firstNum += number;
-    resultNumbers.textContent = `${checkNumLength(firstNum)}`;
-  } else if (operators && secondNum.length < 10) {
-    if (number === ".") {
-      if (!secondNum) {
-        secondNum += "0";
-        resultNumbers.textContent = `${checkNumLength(secondNum)}`;
-        decimalBtn.disabled = true;
-      }
-    }
-    secondNum += number;
-    resultNumbers.textContent = `${checkNumLength(secondNum)}`;
-  }
-};
-
-const acButtonHandler = () => {
-  firstNum = "";
-  operators = "";
-  secondNum = "";
-  mainResult = 0;
-  resultNumbers.textContent = "";
-  resultOperations.textContent = "";
-};
-
-// Must only work when a number is entered.
-// Decimal must be disabled here since because new numbers are
-// entered after.
-// Diving by zero is handled here otherwise, it will interfere with
-// the last if statement black in the mainResultHandler function.
-const operatorsHandler = (currentOperator) => {
-  if (currentOperator === "AC") {
-    acButtonHandler();
-  } else if (operators === "/" && (firstNum === "0" || secondNum === "0")) {
-    acButtonHandler();
-    resultNumbers.textContent = "🤦🏻‍♂️🤦🏽‍♀️";
-  } else if (operators && secondNum) {
-    mainResultHandler();
-  } else if (firstNum) {
-    if (currentOperator === "+/-") {
-      firstNum = `${parseFloat(firstNum) * -1}`;
-      resultNumbers.textContent = checkNumLength(firstNum);
-    } else if (currentOperator === "%") {
-      operators = currentOperator;
-      mainResultHandler(firstNum, (secondNum = 100));
-    } else if (currentOperator != "=") {
-      operators = currentOperator;
-    }
-  }
-};
-
-const delBtnHandler = () => {
-  if (mainResult) {
-    acButtonHandler();
-  } else if (secondNum) {
-    secondNum = secondNum.slice(0, -1);
-    resultNumbers.textContent = `${checkNumLength(secondNum)}`;
-  } else if (firstNum) {
+const deleteCurrentNumber = () => {
+  if (firstNum === resultNumbers.textContent) {
     firstNum = firstNum.slice(0, -1);
-    resultNumbers.textContent = `${checkNumLength(firstNum)}`;
+    resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+    resultOperations.textContent = `${firstNum}`;
+  } else if (secondNum === resultNumbers.textContent) {
+    secondNum = secondNum.slice(0, -1);
+    resultNumbers.textContent = `${styleCurrentNumber(secondNum)}`;
+    resultOperations.textContent = `${firstNum} ${operator} ${secondNum}`;
+  }
+};
+
+const newOperator = (action) => {
+  decimalBtn.disabled = false;
+  decimalCounter = 0;
+  if (firstNum && secondNum) {
+    if ((firstNum === "0" || secondNum === "0") && operator === "/") {
+      deleteAll();
+      resultNumbers.textContent = "😂💀";
+    } else {
+      calculateResult(firstNum, operator, secondNum);
+      resultOperations.textContent = `${firstNum} ${action}`;
+      resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+      operator = action;
+    }
+  } else if (firstNum) {
+    if (action === "%" && firstNum !== "0") {
+      decimalBtn.disabled = true;
+      firstNum = `${firstNum / 100}`;
+      resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+    } else {
+      resultOperations.textContent = `${firstNum} ${action}`;
+      operator = action;
+    }
+  }
+};
+
+const equalEnter = () => {
+  if (firstNum && secondNum) {
+    if ((firstNum === "0" || secondNum === "0") && operator === "/") {
+      deleteAll();
+      resultNumbers.textContent = "😂💀";
+    } else {
+      let tempSecondNum = secondNum;
+      let tempFirstNum = firstNum;
+      calculateResult(firstNum, operator, secondNum);
+      resultOperations.textContent = `${tempFirstNum} ${operator} ${tempSecondNum}`;
+      resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+      operator = "";
+    }
+  }
+};
+
+const updateNumber = (num) => {
+  if (!operator) {
+    if (firstNum.length < 11) {
+      firstNum += num;
+      resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+      resultOperations.textContent = `${firstNum}`;
+    }
+  } else {
+    if (secondNum.length < 11) {
+      secondNum += num;
+      resultNumbers.textContent = `${styleCurrentNumber(secondNum)}`;
+      resultOperations.textContent = `${firstNum} ${operator} ${secondNum}`;
+    }
+  }
+  console.log(firstNum);
+};
+
+const addDecimal = () => {
+  decimalBtn.disabled = true;
+  if (!operator) {
+    if (!firstNum) {
+      updateNumber("0.");
+    } else {
+      updateNumber(".");
+    }
+  } else {
+    if (!secondNum) {
+      updateNumber("0.");
+    } else {
+      updateNumber(".");
+    }
+  }
+};
+
+const oppositeNum = () => {
+  if (!operator) {
+    firstNum = `${parseFloat(firstNum) * -1}`;
+    resultNumbers.textContent = `${styleCurrentNumber(firstNum)}`;
+    resultOperations.textContent = `${firstNum}`;
+    console.log(firstNum);
+  } else {
+    secondNum = `${parseFloat(secondNum) * -1}`;
+    resultNumbers.textContent = `${styleCurrentNumber(secondNum)}`;
+    resultOperations.textContent = `${firstNum} ${operator} ${secondNum}`;
   }
 };
 
@@ -151,20 +159,26 @@ const delBtnHandler = () => {
 const clickKeydownHandler = (event) => {
   console.log(event);
   if (event.type === "keydown" || event.type === "click") {
-    if (event.key === "Backspace" || event.target === delBtn) {
-      delBtnHandler();
+    if (event.target.value === "." || event.key === ".") {
+      addDecimal();
+    } else if (event.target.value === "=" || event.key === "Enter") {
+      equalEnter();
+    } else if (event.target.value === "AC") {
+      deleteAll();
+    } else if (event.target.value === "+/-") {
+      oppositeNum();
+    } else if (event.target === delBtn) {
+      deleteCurrentNumber();
     } else if (
       allNumbers.includes(event.target.value) ||
       allNumbers.includes(event.key)
     ) {
-      console.log(event.target);
-      updateNumbersHandler(event.target.value ?? event.key);
+      updateNumber(event.target.value ?? event.key);
     } else if (
       allOperations.includes(event.target.value) ||
       allOperations.includes(event.key)
     ) {
-      operatorsHandler(event.target.value ?? event.key);
-      decimalBtn.disable = true;
+      newOperator(event.target.value ?? event.key);
     }
   }
   event.target.blur();
